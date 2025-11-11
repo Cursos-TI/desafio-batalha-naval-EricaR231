e96a3506d7110513545bd95646447e176dd2283a
EricaR231 <erykaribeiro.souza@gmail.com>
2025-11-11 23:17:21 +0000

    implementa a logica de dois novos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main(){
 
     char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
@@ -7,6 +8,8 @@ int main(){
    // --- POSICIONAR NAVIOS ---
     int navio1[3]; // navio horizontal
     int navio2[3]; // navio vertical
+    int navio3[3]; // navio diagonal
+    int navio4[3]; // navio diagonal
 
     // Coordenadas iniciais
     int linha1 = 2; // linha do navio horizontal
@@ -15,20 +18,35 @@ int main(){
     int linha2 = 5; // linha do navio vertical
     int col2 = 7;
 
+    int linha3 = 3; // linha do navio diagonal principal
+    int col3 = 2;
+
+    int linha4 = 7; // linha navio diagonal secundária
+    int col4 = 5;
+
     // Preenche os vetores com valor 3 (parte do navio)
     for (int i = 0; i < 3; i++) {
         navio1[i] = 3;
         navio2[i] = 3;
+        navio3[i] = 3;
+        navio4[i] = 3;
     }
 
-    // Posiciona o navio na horizontal no tabuleiro
+    // Posiciona o navio 1 na horizontal no tabuleiro
     for (int i = 0; i < 3; i++) {
         tabuleiro[linha1][col1 + i] = navio1[i];
     }
 
-    // Posiciona o navio na vertical no tabuleiro
+    // Posiciona o navio 2 na vertical no tabuleiro
     for (int i = 0; i < 3; i++) {
         tabuleiro[linha2 + i][col2] = navio2[i];
+    }
+
+    // Posiciona o navio 3 e 4 na diagonal no tabuleiro
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha3 - i][col3 - i] = navio3[i];
+        tabuleiro[linha4 - i][col4 - i] = navio4[i];
+
     }
 
     // --- EXIBE O TABULEIRO ---
@@ -54,5 +72,4 @@ int main(){
     }
 
     return 0;
-
 }
